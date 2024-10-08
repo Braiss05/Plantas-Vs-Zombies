b317640102114151b188679e8972b96f8f102c3e
34648 <34648@LAPTOP-UT54T126>
2024-10-08 12:45:33 +0200

    Creacion de la clase zombie , con los atributos sencillos. Closes #2

--- ProyectoBrais/src/proyectobrais/ProyectoBrais.java
+++ ProyectoBrais/src/proyectobrais/ProyectoBrais.java
@@ -1,26 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyectobrais;
 
-/**
- *
- * @author 34648
- */
 public class ProyectoBrais {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        
         // Prueba del constructor por defecto
-        Zombie z1 = new Zombie();  // Ahora este constructor funcionará
+        Zombie z1 = new Zombie(); 
         z1.mostrarInfo();
 
         // Prueba del constructor paramétrico
-        Zombie z2 = new Zombie("Zombie Yeti", 80, 15, 10);
+        Zombie z2 = new Zombie("Zombie Yeti", 80, 15, 10, "Yeti");
         z2.mostrarInfo();
 
         // Prueba del constructor copia
@@ -32,10 +21,10 @@ public class ProyectoBrais {
         z1.setVida(50);
         z1.setDamage(5);
         z1.setVelocidad(2);
+        z1.setTipo("Caminante");  // Modificamos el tipo de zombie
         z1.mostrarInfo();
 
         // Obtener el número total de zombies creados
         System.out.println("Numero total de zombies: " + Zombie.getNumeroTotalZombies());
     }
-    
 }
